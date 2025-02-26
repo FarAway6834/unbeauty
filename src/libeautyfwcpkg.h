e3233ad3fc3ddd1e3eda0d3e51fb58172d278af5
FarAway6834 <faway6834@gmail.com>
2025-02-26 14:32:56 +0900

    jejejehehehehehehehe

--- src/libeautyfwcpkg.h
+++ src/libeautyfwcpkg.h
@@ -4,6 +4,36 @@
 
 #include "mymcr.h"
 
-PASS;
+/* temp sucks way fix */
+//#undef TS
+//#define TS(...) TP(__VA_ARGS__) ST
+//hehehheehhehehehehhehehehheheheheheheheh
+#undef AA
+#define AA(X) virtual IL X = 0
+#undef TA
+#define TA(...) TP(__VA_ARGS__) US
+/* temp sucks way fix */
+
+#define SELFRETC(T, OPS, G) AO(,OPS,T,G)
+#define SELFRET AP(SELFRETC)
+#define SRA(SRM, OPS) SRM(OPS, LL); SRM(OPS, L); SRM(OPS, D); SRM(OPS, S); SRM(OPS, ULL); SRM(OPS, UD); SRM(OPS, UD); SRM(OPS, us); SRM(OPS, ld); SRM(OPS, LF); SRM(OPS, DO); SRM(OPS, F); SRM(OPS, UC); SRM(OPS, SC); SRM(OPS, C)
+
+/* temp sucks way fix */
+#undef SELFRET
+#define SELFRET(V) SELFRETC(V, CLOSER
+//hehe what is E0274 and E0018? --[1]
+/* temp sucks way fix */
+
+#define AUOP SELFRET(autynum)
+/* temp sucks way fix */
+//[1]
+#undef AUOP
+#define AUOP(OPS, T) SELFRETC(autynum, OPS, T)
+#define SUPADOPA(OPS) SRA(AUOP, OPS)
+/* temp sucks way fix */
+#define IAMPIUNGSIN TBit<autynum, autynum, V>
+ST autynum{
+	SUPADOPA(+); SUPADOPA(-); SUPADOPA(*); SUPADOPA(/); SUPADOPA(^); TV(TN, __optlib_handle__, IAMPIUNGSIN, autynum V);
+};
 
 #endif
--- src/mymcr.h
+++ src/mymcr.h
@@ -26,6 +26,10 @@
 #define PR private
 #define PU public
 #define PO protected
+#define OP operator
+#define SG signed
+//hehehehehehehehe
+#define UG C_(un, SG)
 #define PF PR:
 #define UF PU:
 #define OF PO:
@@ -77,6 +81,8 @@
 #define VI AL(VR)
 #define FI AL(FN)
 
+#define AB(X) VI X = 0
+
 #define NS C_(NA, SP)
 #define TN C_(TY, NA)
 #define TD C_(TY, DF)
@@ -107,11 +113,20 @@
 #define FX LT FL
 #define VX LT VL
 
+#define AA(X) AB(IL X)
+#define AG CE AB
+#define LG CE AA
+#define AS DS AA
+#define AV DS AI
+#define AC LT AI
+#define AX LT AA
+
 #define TC TD CL
 #define MT TD ST
 #define UT TD UN
 #define PT(T) TD T*
-#define FT(N, T, ...) TD T (*N)(__VA_ARGV__)
+#define FN(N, T, ...) T N (__VA_ARGS__)
+#define FT(N, T, ...) TD FN((*N), T, __VA_ARGS__)
 #define AT(T, L) TD T[L]
 
 #define XT(X, ...) TP(__VA_ARGS__) X
@@ -122,11 +137,43 @@
 #define TA TX(US)
 #define TC TX(CL)
 #define CT TX(CP)
+#define TV(T, X, SRC, ...) TA(__VA_ARGS__, T V = SRC) X = V
 
 #define LD US NS
 #define GL(N, V) N::V
 #define GE(N, V) US GL(N, V)
 #define AS(A, B) US A = B
 #define LA(X, N, V) AS(X, GL(N, V))
+
+#define AO(X, Y, T, ...) AA(FN(X##OP##Y, T, __VA_ARGS__))
+
+//hehehehehehehehehehehehheheheheheehehehhehehhhhehehehe
+/*dum fix*/
+#define TYDF typedef
+#define TYNA typename
+#define unSG unsigned
+#undef TS
+#define TS(...) TP(__VA_ARGS__) ST
+/*dum fix*/
+//eeeheehhehehehehehhhehehehhehehehehehheheheehhhehhhhhhhhhehehehehehe
+TD long long LL;
+TD long L;
+TD int D;
+TD short S;
+TD UG long long ULL;
+TD UG long UL;
+TD UG int UD;
+TD UG short us;
+TD long double ld;
+TD long float LF;
+TD double DO;
+TD float F;
+TD UG char UC;
+TD SG char SC;
+TD char C;
+
+TS(TN T, TN G, G B) TBit {
+	T v : B;
+};
 
 #endif
