e1c0c49c0b6ea923ae84dee697acceda40a4c7d8
c0dk1ddy <tax0787bz3@gmail.com>
2025-05-04 15:17:34 +0900

    umk

--- src/autyp.h
+++ src/autyp.h
@@ -2,10 +2,16 @@
 #ifndef _AUTYP_H
 # define _AUTYP_H
 
+#define OPER(X, ...) virtual inline thistyp operator##X##(thistyp) = 0; __VA_OPT__(OPER(__VA_ARGV__))
+
+==,!=,>,<,>=,=<,&,|,^,&&,||
 namespace AUTYP {
 	struct coretyp {
-		s
+		private using thistyp = this auto;
+		OPER(+, -, *, /, ^, ==, !=, >, <, >=, =<, &, |, &&, ||)
 	}
 }
+
+#undef OPER
 
 #endif
